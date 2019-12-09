749908e0752e70f0ae3c86b43bffd9929d95f098
SonSang <shh1295@gmail.com>
2019-12-09 10:35:02 +0900

    Added minor fix.

--- main.cpp
+++ main.cpp
@@ -560,8 +560,8 @@ void SRmenu() {
     ImGui::Begin("Shape Retrieval menu", &SRmenu_on);
     // if(ImGui::CollapsingHeader("Model"))
     //     SRmenu_model();
-    if(ImGui::CollapsingHeader("Search"))
-        SRmenu_search();
+    // if(ImGui::CollapsingHeader("Search"))
+        // SRmenu_search();
     // if(ImGui::CollapsingHeader("EMD"))
     //    SRmenu_EMD();
     SRmenu_final();
@@ -716,7 +716,7 @@ std::shared_ptr<AF::object> searchModel;
 std::vector<std::string> searchResult;
 bool selectModelInit = true;
 
-void selectSearchModel(const std::string &path) {
+void selectSearchModel(std::string &path) {
 
     if(selectModelInit) {
         searchModel = std::make_shared<AF::object>(path);
@@ -733,7 +733,23 @@ void selectSearchModel(const std::string &path) {
         tptr = std::make_shared<AF::SRsphere_tree>();
 
     ptr->set_shader(globalShader);
-    ptr->get_geometry().build_obj(path);
+    try {
+        ptr->get_geometry().build_obj(path);
+    } catch(std::exception &e) {
+        std::string npath = path;
+        for(int i = 0; i < npath.length(); i++) {
+            if(npath.substr(i, 7) == std::string("/Assets")) {
+                npath = npath.substr(i);
+                namespace fs = std::experimental::filesystem;
+                fs::path valPath = fs::current_path();
+                valPath /= npath;
+                path = valPath;
+                break;
+            }
+        }
+        ptr->get_geometry().build_obj(path);
+    }
+    
     ptr->get_geometry().scale_norm();
     ptr->get_geometry().compute_normals();
     ptr->build_BO();
